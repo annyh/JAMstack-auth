5275fbce74f45665d42ac99b6673aeb9a70e1398
annyh <annyh@sfu.ca>
2020-10-28 22:27:51 -0700

    change package.json

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@ import './layout.css'
 import { IdentityContextProvider } from 'react-netlify-identity-widget'
 
 const Layout = ({ children }) => (
-    <IdentityContextProvider url='https://jamstack-intro-auth.netlify.com'>
+    <IdentityContextProvider url='https://jamstack-intro-auth-anny.netlify.com'>
         <header>
             <Link to="/">JAMstack app</Link>
             <main>{children}</main>
