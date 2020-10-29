97144d681d82e7ab8b40fde1b931f8c7cf746234
annyh <annyh@sfu.ca>
2020-10-28 22:22:29 -0700

    initial commit

---
+++ src/components/route-base.js
@@ -0,0 +1,3 @@
+import React from 'react'
+const RouteBase = () => <h1>All your base belongs to us</h1>
+export default RouteBase
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Link} from 'gatsby'
+import Layout from '../components/layout'
+
+export default () => (
+    <Layout>
+    <h1>Login to find out X</h1>
+    <Link to="/dashboard">Go to dashboard</Link>
+    </Layout>
+)
